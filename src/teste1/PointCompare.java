ab3154b4f87d51108498ddc95ba678b35ac214f0
gongas13 <gpinheiro93@gmail.com>
2017-03-12 14:35:25 +0000

    comparator

---
+++ src/teste1/PointCompare.java
@@ -0,0 +1,18 @@
+package voltinhas;
+
+import java.awt.Point;
+import java.util.Comparator;
+
+public class PointCompare implements Comparator<Point> {
+    public int compare(Point a, Point b) {
+        if (a.getX() < b.getX()) {
+            return -1;
+        }
+        else if (a.getX() > b.getX()) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
